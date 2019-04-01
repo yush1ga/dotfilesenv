1ab23e1485eea9716933b032f7b6d1faac62fc51
Yuki Shiga <y.shiga.91+yush1ga@gmail.com>
2019-04-02 02:25:28 +0900

    Update list command

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import json
+
 from setuptools import setup
+
+with open('./Pipfile.lock') as f:
+    install_requires = []
+    for k, v in json.load(f).items():
+        if k == 'default':
+            for l, d in v.items():
+                install_requires.append(l+d['version'])
 
 setup(
     name='dotfilesenv',
-    version='0.0.2',
+    version='0.0.3',
     py_modules=['dotfilesenv'],
-    install_requires=[
-        'Click',
-    ],
+    install_requires=install_requires,
     entry_points='''
         [console_scripts]
         dotfilesenv=dotfilesenv:main
