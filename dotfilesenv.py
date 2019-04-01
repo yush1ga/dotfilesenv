5636ccaa1fcd9d800ed68fc0a34129c317a76ab9
yush1ga+circleci <yush1ga+circleci@users.noreply.github.com>
2019-04-01 19:04:16 +0000

    [ci skip] [auto] bump up version

--- dotfilesenv.py
+++ dotfilesenv.py
@@ -10,7 +10,7 @@ import click
 from typing import Dict
 
 # version information
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 # setting information
 DOTFILESENV_PATH = os.path.join(os.environ.get('HOME'), '.dotfilesenv')
